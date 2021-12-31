23fd8b7b4f8845203b4bdaf1d936f5b3ad91529c
Catzzz <chuneleu@gmail.com>
2021-12-30 20:27:07 -0800

    modify setup-Linux-amazn-2.sh and test

--- build-aux/setup-Linux-amzn-2.sh
+++ build-aux/setup-Linux-amzn-2.sh
@@ -7,8 +7,9 @@ chmod -R 775 /usr/local
 chown -R root:adm /usr/local
 
 # Install needed system tools
-yum -q update -y ; 
 yum -q clean all
+yum -q update -y ; 
+
 yum -q groupinstall "Development Tools" -y
 yum -q install cmake -y 
 yum -q install ncurses-devel -y
@@ -36,28 +37,37 @@ if [ ! -x /usr/local/bin/python3 -o "$(/usr/local/bin/python3 --version | cut -f
 	/usr/local/bin/python3 -m pip install wheel 
 	/usr/local/bin/python3 -m pip install censusdata
 fi
-
+echo "Install mono"
+yum -q install mono-devel -y
 # mono
-if [ ! -f /usr/bin/mono ]; then
-	rpmkeys --import "http://keyserver.ubuntu.com/pks/lookup?op=get&search=0x3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF"
-	su -c 'curl https://download.mono-project.com/repo/centos8-stable.repo | tee /etc/yum.repos.d/mono-centos8-stable.repo'
-	yum -q install mono-devel -y
-fi
+# if [ ! -f /usr/bin/mono ]; then
+# 	#https://gist.github.com/yetanotherchris/42b429059e5fe1b3f7bb4169f5706c00
+# 	# rpmkeys --import "http://keyserver.ubuntu.com/pks/lookup?op=get&search=0x3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF"
+# 	# su -c 'curl https://download.mono-project.com/repo/centos8-stable.repo | tee /etc/yum.repos.d/mono-centos8-stable.repo'
+# 	# yum -q install mono-devel -y
+# fi
 
 # natural_docs
-if [ ! -x /usr/local/bin/natural_docs ]; then
-	cd /usr/local
-	curl https://www.naturaldocs.org/download/natural_docs/2.0.2/Natural_Docs_2.0.2.zip > natural_docs.zip
-	unzip -qq natural_docs
-	rm -f natural_docs.zip
-	mv Natural\ Docs natural_docs
-	echo '#!/bin/bash
-mono /usr/local/natural_docs/NaturalDocs.exe \$*' > /usr/local/bin/natural_docs
-	chmod a+x /usr/local/bin/natural_docs
-fi
+# if [ ! -x /usr/local/bin/natural_docs ]; then
+# 	cd /usr/local
+# 	curl https://www.naturaldocs.org/download/natural_docs/2.0.2/Natural_Docs_2.0.2.zip > natural_docs.zip
+# 	unzip -qq natural_docs
+# 	rm -f natural_docs.zip
+# 	mv Natural\ Docs natural_docs
+# 	echo '#!/bin/bash
+# mono /usr/local/natural_docs/NaturalDocs.exe \$*' > /usr/local/bin/natural_docs
+# 	chmod a+x /usr/local/bin/natural_docs
+# fi
 
 # converter support
 # cd /tmp
 # curl http://download-ib01.fedoraproject.org/pub/epel/7/x86_64/Packages/m/mdbtools-0.7.1-3.el7.x86_64.rpm > mdbtools-0.7.1-3.el7.x86_64.rpm
 # rpm -Uvh mdbtools-0.7.1-3.el7.x86_64.rpm
+echo "Install support"
+cd ~
+amazon-linux-extras install epel -y
+yum-config-manager --enable epel
 yum -q install mdbtools -y
+#latex
+echo "Install latex"
+yum -q install texlive -y
